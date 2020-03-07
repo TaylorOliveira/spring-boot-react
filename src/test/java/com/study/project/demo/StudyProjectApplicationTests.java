aab893b24716449afa4ca65906089c083a82e596
Taylor Oliveira <taylorsantosoliveira@hotmail.com>
2020-03-07 10:56:42 -0300

    initial commit

---
+++ src/test/java/com/study/project/demo/StudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
